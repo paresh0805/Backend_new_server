b93cd7729ea6c9b8b23ae9460772a4e6502d5a62
Paresh Madav <madavparesh08@gmail.com>
2025-09-20 10:42:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,16 @@
-const mongoose=require('mongoose')
-const connection=mongoose.connect("mongodb+srv://sandhukirat482:kirat9855@cluster0.bbnbwwp.mongodb.net/").then(()=>{
-     console.log("connected");
+// const mongoose=require('mongoose')
+// const connection=mongoose.connect("mongodb+srv://sandhukirat482:kirat9855@cluster0.bbnbwwp.mongodb.net/").then(()=>{
+//      console.log("connected");
     
- })
- module.exports=connection
+//  })
+//  module.exports=connection
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+module.exports=connection
